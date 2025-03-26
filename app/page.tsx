2b5670e572a1cab4daa65ce19adb0638b9bfcc09
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-03-26 18:38:26 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,106 @@
-import Image from "next/image";
+'use client'
+import BouldersCom from "@/components/BouldersCom";
+import HandRecognizer from "@/components/HandRecognizer";
+import RocketComponets from "@/components/RocketComponets";
+import { useEffect, useRef, useState } from "react";
+
+let generationInterval: any;
+let removerInterval: any;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [rocketLeft, setRocketLeft] = useState(0);
+  const [isDetected, setIsDetected] = useState(false);
+  const [degrees, setDegrees] = useState(0);
+  const [boulders, setBulders] = useState<any[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [rocket, setRocket] = useState<any>();
+  const rocketRef = useRef(null);
+
+  useEffect(() => {
+    setRocketLeft(window.innerWidth / 2)
+  }, [])
+
+  useEffect(() => {
+    if (isDetected) {
+
+      generationInterval = setInterval(() => {
+        setBulders(prevArr => {
+          let retArr = [...prevArr];
+          for (let i = 0; i < 4; i++) {
+            const now = Date.now();
+            retArr = [...retArr, {
+              timeStamp: now,
+              key: `${now} - ${Math.random()}`
+            }]
+          }
+          return retArr
+        })
+      }, 1000);
+
+      removerInterval = setInterval(() => {
+        const now = Date.now();
+        setBulders(prevArr => {
+          return prevArr.filter((b, idx) => {
+            return (now - b.timeStamp) < 5000
+          })
+        })
+      }, 5000)
+    }
+
+    return () => {
+      clearInterval(generationInterval);
+      removerInterval(removerInterval);
+    }
+  }, [isDetected])
+
+
+  const setHandResults = (result: any) => {
+
+    setIsDetected(result.isDetected);
+    setDegrees(result.degrees);
+
+    // if (result.degrees && result.degrees !== 0)
+    if (typeof result.degrees === "number" && result.degrees !== 0) {
+      setRocketLeft(prev => {
+        const ret = prev - result.degrees / 6;
+
+        if (ret < 20) {
+          return prev;
+        }
+
+        if (ret > window.innerWidth - 52) {
+          return prev;
+        }
+
+        return ret;
+      })
+    }
+    setRocket(((rocketRef.current as any).getBoundingClientReact()));
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
+      <div className="absolute left-3 top-3 z-30 w-24">
+        <HandRecognizer setHandResults={setHandResults} />
+      </div>
+      <div
+        ref={rocketRef}
+        id='rocket-container'
+        style={{
+          position: 'absolute',
+          left: rocketLeft,
+          transition: 'all',
+          animationDuration: '10ms',
+          marginTop: '350px'
+        }}>
+        <RocketComponets degrees={degrees} />
+      </div>
+
+      <div className="absolute z-10 h-screen w-screen overflow-hidden">
+        {boulders.map((b, index) => {
+          return <BouldersCom key={b.key} isMooving={isDetected} />
+        })}
+      </div>
+    </main>
   );
 }
